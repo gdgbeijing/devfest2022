3f318d66d3d101b914aad9016931b926507656a9
Nan YE <dailystudio2010@gmail.com>
2022-10-28 23:03:15 +0800

    adapt to Jetpack Compose 1.3.2 + Material Design 3; use composeBom to resolve dependencies;

--- AndroidWorkshop/ComposeDynamicIsland/app/src/main/java/com/compose/dynamicisland/MainActivity.kt
+++ AndroidWorkshop/ComposeDynamicIsland/app/src/main/java/com/compose/dynamicisland/MainActivity.kt
@@ -6,21 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.compose.dynamicisland.ui.theme.ComposeDynamicIslandTheme
 
@@ -32,7 +29,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colorScheme.background
                 ) {
 
                     var state: IslandType by remember {
